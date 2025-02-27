a725a6726673522cf5120e9a726857a75ff02f4f
Bereket <bereketalemayehuf@gmail.com>
2025-02-27 14:10:20 -0600

    updated Image and Actions

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"food-recipes/backend/internal/actions"
+	"net/http"
+	"os"
+	"strconv"
+)
 
 func main() {
-    fmt.Println("Go backend is running!")
+    http.HandleFunc("/signup", actions.SignupHandler)
+    http.HandleFunc("/login", actions.LoginHandler)
+    http.HandleFunc("/recipes", actions.CreateRecipeHandler)
+    http.HandleFunc("/recipes/update", actions.UpdateRecipeHandler)
+    http.HandleFunc("/recipes/delete", actions.DeleteRecipeHandler)
+    http.HandleFunc("/upload", actions.UploadFileHandler)
+
+    portStr := os.Getenv("PORT")
+    if portStr == "" {
+        fmt.Println("PORT environment variable not set, using default port 8081")
+        portStr = "8081" // Default port if not set
+    }
+
+    port, err := strconv.Atoi(portStr)
+    if err != nil {
+        fmt.Printf("Error converting PORT to integer: %v\n", err)
+        return // Exit if port conversion fails
+    }
+
+    fmt.Printf("Server listening on port %d\n", port)
+    err = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+    if err != nil {
+        fmt.Printf("Error starting server: %v\n", err)
+    }
 }
--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,13 @@ module food-recipes/backend
 
 go 1.24.0
 
-require github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+require github.com/machinebox/graphql v0.2.2
+
+require github.com/matryer/is v1.4.1 // indirect
+
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.1
+	github.com/google/uuid v1.6.0
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.35.0
+)
